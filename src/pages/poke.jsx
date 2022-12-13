19c07c769d6a4b74d4bfd6c07c2ebed50673b55b
ri-factor <ri@3sidedcube.com>
2022-12-13 12:15:33 +0000

    Upload Project

---
+++ src/pages/poke.jsx
@@ -0,0 +1,192 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+const pokeEndPoint = "https://pokeapi.co/api/v2/pokemon";
+const pokemonEndPoint = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=1154";
+
+const Poke = ({ formatName }) => {
+  const { pokeId } = useParams();
+  const [pokeState, setPokeState] = useState([]);
+  const [secondPokeState, setSecondPokeState] = useState([]);
+  const [pokemonState, setPokemonState] = useState([]);
+  const [isComparisonState, setIsComparisonState] = useState(false);
+
+  useEffect(() => {
+    fetch(`${pokeEndPoint}/${pokeId}`).then((res) => {
+      res.json().then((data) => {
+        setPokeState(data);
+        fetch(pokemonEndPoint).then((res) => {
+          res.json().then((data) => {
+            setPokemonState(data.results);
+          });
+        });
+      });
+    });
+  }, [pokeId]);
+
+  const handleComparePokemon = () => {
+    setIsComparisonState(true);
+  };
+
+  const handleCancelComparison = () => {
+    setIsComparisonState(false);
+    setSecondPokeState([]);
+  };
+
+  const handleSecondPokemonSelected = (event) => {
+    const secondPokeId = Number(event.currentTarget.value);
+    fetch(`${pokeEndPoint}/${secondPokeId}`).then((res) => {
+      res.json().then((data) => {
+        setSecondPokeState(data);
+      });
+    });
+  };
+
+  const formatHeightOrWeight = (value) => {
+    return (value / 10).toFixed(1);
+  };
+
+  if (isComparisonState) {
+    return (
+      <>
+        <div>
+          <button
+            className="btn btn-danger m-3"
+            onClick={handleCancelComparison}
+          >
+            Cancel
+          </button>
+        </div>
+        <div className="d-flex flex-wrap pokemon-grid">
+          Poke detail
+          <div className="card m-3" style={{ width: 400 }}>
+            <img
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeId}.png`}
+              onError={(e) => {
+                e.target.onError = null;
+                e.target.src =
+                  "https://cdn.pixabay.com/photo/2016/09/01/09/31/pokemon-1635610_1280.png";
+              }}
+              className="card-image-top"
+              alt={pokeState.name} // to consider better alt text, image is uncessary
+            ></img>
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Height (m)</th>
+                  <th>Weight (kg)</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{formatName(pokeState.name)}</td>
+                  <td>{formatHeightOrWeight(pokeState.height)}</td>
+                  <td>{formatHeightOrWeight(pokeState.weight)}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="card m-3" style={{ width: 400 }}>
+            <img
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${secondPokeState}.png`}
+              onError={(e) => {
+                e.target.onError = null;
+                e.target.src =
+                  "https://cdn.pixabay.com/photo/2016/09/01/09/31/pokemon-1635610_1280.png";
+              }}
+              className="card-image-top"
+              alt={secondPokeState.name} // to consider better alt text, stating 'image' is uncessary
+            ></img>
+
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Height (m)</th>
+                  <th>Weight (kg)</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{formatName(secondPokeState.name)}</td>
+                  <td>{formatHeightOrWeight(secondPokeState.height)}</td>
+                  <td>{formatHeightOrWeight(secondPokeState.weight)}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <div>
+        Select a Pokemon to Compare
+        <select onChange={handleSecondPokemonSelected}>
+          {" "}
+          <option>Pokemon</option>
+          {pokemonState?.map((poke) => {
+            return (
+              <option
+                key={poke.url}
+                value={
+                  poke.url.includes("pokemon-species")
+                    ? poke.url.match(/(?<=pokemon-species\/)\d[^\/]*/)[0]
+                    : poke.url.match(/(?<=pokemon\/)\d[^\/]*/)[0]
+                }
+              >
+                {formatName(poke.name)}
+              </option>
+            );
+          })}
+          <option></option>
+        </select>
+      </div>
+
+      <div>
+        <button className="btn btn-info m-3" onClick={handleComparePokemon}>
+          Compare
+        </button>
+      </div>
+      <div>
+        Poke detail
+        <div>
+          <div className="card m-3" style={{ width: 400 }}>
+            <img
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeId}.png`}
+              onError={(e) => {
+                e.target.onError = null;
+                e.target.src =
+                  "https://cdn.pixabay.com/photo/2016/09/01/09/31/pokemon-1635610_1280.png";
+              }}
+              className="card-image-top"
+              alt={pokeState.name} // to consider better alt text, stating 'image' is uncessary
+            ></img>
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Height (m)</th>
+                  <th>Weight (kg)</th>
+                  {/* <th>Other</th> */}
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{formatName(pokeState.name)}</td>
+                  <td>{formatHeightOrWeight(pokeState.height)}</td>
+                  <td>{formatHeightOrWeight(pokeState.weight)}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Poke;
---
+++ src/pages/pokemon.jsx
@@ -0,0 +1,174 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// can update limit as required [1154]
+const pokemonEndPoint = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=1154";
+
+const Pokemon = ({ onAddToFavourites, onFavouriteClass, formatName }) => {
+  const [pokemonState, setPokemonState] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedGeneration, setSelectedGeneration] = useState("All");
+
+  useEffect(() => {
+    fetch(pokemonEndPoint).then((res) => {
+      res.json().then((data) => {
+        setPokemonState(data.results);
+      });
+    });
+  }, []);
+
+  let filteredPokemon = pokemonState;
+  if (searchQuery) {
+    filteredPokemon = pokemonState.filter((pokemon) =>
+      pokemon.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+  }
+
+  const displayGenerationStatement = () => {
+    if (selectedGeneration === "All") {
+      return "Showing all Generations";
+    } else {
+      return `Showing Generation ${selectedGeneration}`;
+    }
+  };
+
+  const handleGenerationSelected = (g) => {
+    if (selectedGeneration === g) {
+      fetch(pokemonEndPoint).then((res) => {
+        res.json().then((data) => {
+          setPokemonState(data.results);
+          setSelectedGeneration("All");
+        });
+      });
+      return;
+    }
+    const generation = Number(g);
+    fetch(`https://pokeapi.co/api/v2/generation/${generation}`).then((res) => {
+      res.json().then((data) => {
+        setPokemonState(data.pokemon_species);
+        setSelectedGeneration(g);
+      });
+    });
+  };
+
+  const pokeSlug = (pokeUrl) => {
+    return `/pokemon/${
+      pokeUrl.includes("pokemon-species")
+        ? pokeUrl.match(/(?<=pokemon-species\/)\d[^]*/)[0]
+        : pokeUrl.match(/(?<=pokemon\/)\d[^]*/)[0]
+    }`;
+  };
+
+  const pokeId = (pokeUrl) => {
+    return pokeUrl.includes("pokemon-species")
+      ? pokeUrl.match(/(?<=pokemon-species\/)\d[^\/]*/)[0]
+      : pokeUrl.match(/(?<=pokemon\/)\d[^\/]*/)[0];
+  };
+
+  return (
+    <>
+      <div>
+        <input
+          type="text"
+          name="search-pokemon"
+          id="search-pokemon"
+          placeholder="Search Pokemon..."
+          className="search-input m-3"
+          value={searchQuery}
+          onChange={(event) => {
+            setSearchQuery(event.currentTarget.value);
+          }}
+        ></input>
+      </div>
+
+      <div>
+        <p>{displayGenerationStatement()}</p>
+        <button
+          onClick={() => handleGenerationSelected(1)}
+          className="btn btn-dark m-2"
+        >
+          Generation 1
+        </button>
+        <button
+          onClick={() => handleGenerationSelected(2)}
+          className="btn btn-dark m-2"
+        >
+          Generation 2
+        </button>
+        <button
+          onClick={() => handleGenerationSelected(3)}
+          className="btn btn-dark m-2"
+        >
+          Generation 3
+        </button>
+        <button
+          onClick={() => handleGenerationSelected(4)}
+          className="btn btn-dark m-2"
+        >
+          Generation 4
+        </button>
+        <button
+          onClick={() => handleGenerationSelected(5)}
+          className="btn btn-dark m-2"
+        >
+          Generation 5
+        </button>
+        <button
+          onClick={() => handleGenerationSelected(6)}
+          className="btn btn-dark m-2"
+        >
+          Generation 6
+        </button>
+        <button
+          onClick={() => handleGenerationSelected(7)}
+          className="btn btn-dark m-2"
+        >
+          Generation 7
+        </button>
+        <button
+          onClick={() => handleGenerationSelected(8)}
+          className="btn btn-dark m-2"
+        >
+          Generation 8
+        </button>
+      </div>
+
+      <div className="d-flex flex-wrap pokemon-grid">
+        {filteredPokemon?.map((poke) => {
+          return (
+            <div key={poke.url}>
+              {" "}
+              <div className="card m-3" style={{ width: 200 }}>
+                <img
+                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeId(
+                    poke.url
+                  )}.png`}
+                  onError={(e) => {
+                    e.target.onError = null;
+                    e.target.src =
+                      "https://cdn.pixabay.com/photo/2016/09/01/09/31/pokemon-1635610_1280.png";
+                  }}
+                  className="card-image-top"
+                  alt={poke.name} // to consider better alt text, stating 'image' is unnecessary
+                ></img>
+                <div>
+                  <Link to={pokeSlug(poke.url)} className="btn btn-link m-3">
+                    {formatName(poke.name)}
+                  </Link>
+
+                  <i
+                    onClick={() => onAddToFavourites(poke)}
+                    className={onFavouriteClass(poke.name)}
+                  ></i>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Pokemon;
